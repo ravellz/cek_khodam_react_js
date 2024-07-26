98f4a80b46924965d751e439cf633ae652feb108
ravellz <ravellz123@gmail.com>
2024-07-27 01:55:48 +0700

    Add to database feature

--- src/generator.jsx
+++ src/generator.jsx
@@ -1,27 +1,43 @@
 import "./generator.css";
 import React, { useState } from "react";
+import axios from "axios";
 
 const Generator = () => {
-  const [inputValue, setInputValue] = useState("");
+  // Untuk input nama orangnya
+  const [nama, setNama] = useState("");
+  // Untuk ganti dari button Cek Khodam ke Cek Khodam Nama Yang Lain
   const [isButtonSwitcher, setIsButtonSwitcher] = useState(true);
+  // Bikin kolom input jadi gabisa diisi biar kalau mau isi lagi harus pencet tombol Cek Khodam Nama Yang Lain
   const [isDisabled, setIsDisabled] = useState(false);
 
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
+  // Untuk ganti value nama dari kosong ("") ke nama orangnya
+  const handleNama = (event) => {
+    setNama(event.target.value);
   };
 
-  // const handleInputDisabled = () => {
-  //   setIsDisabled(!isDisabled);
-  // };
-
+  // Tombol Cek Khodam yang multifungsi
   const handleClick = (el) => {
-    if (inputValue == "evan" || inputValue == "Evan") {
-      document.getElementById("display").innerHTML = "CIEE DINAAAA";
+    if (nama == "evan" || nama == "Evan") {
+      let hasilKhodam = "Dina My Lop";
+      document.getElementById("display").innerHTML = hasilKhodam;
       setIsButtonSwitcher(!isButtonSwitcher);
       setIsDisabled(!isDisabled);
-    } else if (inputValue == "") {
+
+      // axios untuk nyambungin ke database pake php
+      const url = "http://localhost/db_khodam/koneksi.php";
+      let fData = new FormData();
+      // kayaknya append ini gunanya untuk passing data ke php, valuenya bebas yang penting yang di dalam petik "blabla" itu sama kayak di php bagian $nama = $_POST[' blabla'];
+      fData.append("nama", nama);
+      fData.append("khodam", hasilKhodam);
+      axios
+        .post(url, fData)
+        .then((response) => alert(response.data))
+        .catch((error) => alert(error));
+    } else if (nama == "") {
+      // validasi input biar ngga kosong
       alert("Masukkan Nama Terlebih Dahulu");
     } else {
+      // Randomizer untuk randomize kata yang terdiri dari kata depan + kata belakang yang diacak sehingga jadi hasilKhodam
       let depan = [
         "Babi",
         "Unta",
@@ -48,19 +64,32 @@ const Generator = () => {
         "Kayang",
       ];
 
-      let modDepan = Math.floor(Math.random() * depan.length);
-      let modBelakang = Math.floor(Math.random() * belakang.length);
-      document.getElementById("display").innerHTML = depan[modDepan].concat(
-        " ",
-        belakang[modBelakang]
-      );
-      console.log(depan[modDepan], belakang[modBelakang]);
+      let kataDepan = Math.floor(Math.random() * depan.length);
+      let kataBelakang = Math.floor(Math.random() * belakang.length);
+      let hasilKhodam = depan[kataDepan].concat(" ", belakang[kataBelakang]);
+      document.getElementById("display").innerHTML = hasilKhodam;
+
+      // Block code axios untuk menyambungkan dan passing data ke koneksi.php biar bisa dipakai disana
+      const url = "http://localhost/db_khodam/koneksi.php";
+      let fData = new FormData();
+      // "nama" disini bukan diambil dari id atau classname seperti metode koneksi ke php vanilla, tapi sebagai inisial yang dapat dipanggil di koneksi.php.
+      // Karena hal diataslah sebenarnya ngga perlu nyamain id/classname nya kayak metode POST biasa di php, jadi value yang kanan itu bisa ngambil dari variabel apa aja (bisa useState atau variabel langsung dari blok kode logika/matematika. cth randomizer yang ada diatas.)
+      fData.append("nama", nama);
+      // hasilKhodam disini diambil dari pure hasil randomizer dan ngga pake useState. 3 hari nyari errornya, ternyata solusinya semudah ngga usah pake useState karena ngga harus ngambil value dari input yang bergantung pada id/classname nya untuk metode POST :')
+      fData.append("khodam", hasilKhodam);
+      axios
+        .post(url, fData)
+        .then((response) => alert(response.data))
+        .catch((error) => alert(error));
+
       setIsButtonSwitcher(!isButtonSwitcher);
       setIsDisabled(!isDisabled);
     }
+    // Bikin tombolnya ngga langsung reset karena bentuk tombolnya input bukan button yang punya default nge-refresh page
     el.preventDefault();
   };
 
+  // Fungsi untuk refresh page biar bisa ulang dari awal isi nama orangnya
   const refreshButton = () => {
     window.location.reload(false);
   };
@@ -70,14 +99,15 @@ const Generator = () => {
       <div className="body">
         <div className="flex">
           <h1 className="title">Cek Khodam</h1>
-          <form action="">
+          <form>
             <input
               id="nama"
               className="nama"
               type="text"
               placeholder="Masukkan Nama"
-              onChange={handleInputChange}
-              value={inputValue}
+              onChange={handleNama}
+              value={nama}
+              // disabled isinya true or false untuk aktif dan non-aktifin fitur disablednya
               disabled={isDisabled}
             />
             <div className="show" id="display">
